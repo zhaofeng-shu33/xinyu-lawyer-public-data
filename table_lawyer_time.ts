f2e53c642d00c1550d4b69939cb2289296281ac1
zhaofeng-shu33 <616545598@qq.com>
2020-03-03 22:35:43 +0800

    update js

--- table_lawyer_time.ts
+++ table_lawyer_time.ts
@@ -1,26 +1,28 @@
 import { TableConfig, getCellByName } from './table';
-const lawyerTable: TableConfig = {
+const lawyerTimeTable: TableConfig = {
   guid: 'qZx7ltnDo0I9BSLD',
   indexKey: 'lawyer',
-  sheets: ['志愿者服务时长排序表'],
+  sheets: ['律所总时数公示'],
   skipRows: 1,
   skipColumns: 0,
   nameRow: 1,
   typeRow: -1,
   defaultValueRow: -1,
-  maxColumn: 'C',
-  getFilePath: () => 'lawyer/data.json',
+  maxColumn: 'E',
+  getFilePath: () => 'lawyer-time/data.json',
   feParser: async (data: any[]) => {
     return data.map((row, id) => {
       return {
         id,
         name: getCellByName(row, '姓名').value,
         hours: getCellByName(row, '总服务时长').value,
-        office: getCellByName(row, '所在单位').value
-      };
+        office: getCellByName(row, '所在单位').value,
+        office_time: getCellByName(row, '单位服务时长').value,
+        office_number: getCellByName(row, '单位服务人数').value
+    };
     });
   },
   validation: () => true
 };
 
-export default lawyerTable;
+export default lawyerTimeTable;
